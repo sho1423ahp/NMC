9418f6dd8cb1d87b5ade1468647f5ffdc604759c
n-movie-creator <n.movie.creator@gmail.com>
2017-07-14 11:17:59 +0900

    comment

--- js/jquery.js
+++ js/jquery.js
@@ -10267,3 +10267,16 @@ $(function(){
       return false;
    });
 });
+
+$(function(){
+    $(window).scroll(function (){
+        $('.fadein').each(function(){
+            var elemPos = $(this).offset().top;
+            var scroll = $(window).scrollTop();
+            var windowHeight = $(window).height();
+            if (scroll > elemPos - windowHeight + 200){
+                $(this).addClass('scrollin');
+            }
+        });
+    });
+});
